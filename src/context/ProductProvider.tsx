0d250264808adf287f90351f3e2a3e41970c2694
Rahul <rahulsharma150100@gmail.com>
2024-07-30 16:31:23 +0530

    Initial commit

---
+++ src/context/ProductProvider.tsx
@@ -0,0 +1,42 @@
+import { Children, createContext, ReactElement, useState } from "react";
+
+export type ProductType ={
+    sku:string;
+    name:string;
+    price:number;
+}
+
+const initState: ProductType[]=[
+    {
+        "sku": "item0001",
+        "name": "Widget",
+        "price": 9.99
+    },
+    {
+        "sku": "item0002",
+        "name": "Premium Widget",
+        "price": 19.99
+    },
+    {
+        "sku": "item0003",
+        "name": "Deluxe Widget",
+        "price": 29.99
+    }
+]
+export type UseProductsContextType={products: ProductType[]}
+
+const initContextState:UseProductsContextType={
+    products:[]
+}
+const ProductContext= createContext<UseProductsContextType>(initContextState);
+type ChildrenType={children?:ReactElement| ReactElement[]}
+
+export const ProductsProvider=({children}:ChildrenType): ReactElement=>{
+    const[products,setProducts]=useState<ProductType[]>(initState)
+    return(
+        <ProductContext.Provider value={{products}}>
+            {children}
+        </ProductContext.Provider>
+    )
+}
+export default ProductContext 
